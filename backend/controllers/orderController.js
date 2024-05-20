99cf50d2288bfebbbf1d18452b9d2286767baa55
Sujit Kumar <skr87981@gmail.com>
2024-05-21 02:33:48 +0530

    created order update feature

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = 'http://localhost:5173';
+    const frontend_url = 'http://localhost:5174';
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -99,4 +99,15 @@ const listOrders = async (req, res) => {
     }
 }
 
-export { placeOrder,verifyOrder,userOrders, listOrders };
+// api for updating order status
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
+        res.json({ success: true, message: 'Status updated successfully' });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: 'Error' });
+    }
+}
+
+export { placeOrder,verifyOrder,userOrders, listOrders, updateStatus };
